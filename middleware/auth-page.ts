b8374054b2f9622275457f4044c64b0a63d8553e
Keji Ojo <ojokeji@gmail.com>
2024-06-09 23:52:16 +0100

    chore: updated routes

--- middleware/auth-page.ts
+++ middleware/auth-page.ts
@@ -2,17 +2,17 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const auth = useAuth().value
 
   // User is not loggedIn and visited route is logout
-  if (to.path === '/logout' && !auth.isLoggedIn)
-    return navigateTo(from.path === '/logout' ? '/login' : from)
+  if (to.path === '/auth/sign-out' && !auth.isLoggedIn)
+    return navigateTo(from.path === '/auth/sign-out' ? '/' : from)
 
   // User is loggedIn and visited route is logout
-  if (to.path === '/logout') {
+  if (to.path === '/auth/sign-out') {
     await usePost('/auth/logout', {})
 
-    return navigateTo('/login')
+    return navigateTo('/')
   }
 
   // User is loggedIn and visited route is login
-  if (to.path === '/login' && useAuth().value.isLoggedIn)
-    return navigateTo(from.path === '/login' ? '/' : from)
+  //   if (to.path === '/login' && useAuth().value.isLoggedIn)
+  //     return navigateTo(from.path === '/login' ? '/' : from)
 })
