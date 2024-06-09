008b0a4f22a0630bf5ee1045cc9731ea2e969dc5
Keji Ojo <ojokeji@gmail.com>
2024-06-09 23:49:30 +0100

    update: updated fetch function

--- composables/useFetchExtended.ts
+++ composables/useFetchExtended.ts
@@ -6,11 +6,11 @@ export const useFetchExtended = <T>(
   url: string,
   options: UseFetchOptions<T> = {},
   payload: Record<string, any> = {},
+  bypassBearer: Record<string, any> = {},
+  baseUrlType: string = '',
 ) => {
   const authCookie = useCookie<Auth>('auth_cookie')
   const auth = useAuth()
-
-  const toast = useToast({ props: { position: 'topEnd', timeout: '1s' } })
   const fetchState = useFetchState(url)
   const config = useRuntimeConfig()
 
@@ -22,7 +22,9 @@ export const useFetchExtended = <T>(
     // Key - for caching
     key: url,
 
-    headers: auth.value.accessToken ? { Authorization: `Bearer ${auth.value.accessToken}` } : {},
+    headers: bypassBearer.token
+      ? { Authorization: `Bearer ${bypassBearer.token}` }
+      : { Authorization: `Bearer ${auth.value.accessToken}` },
 
     onRequest: () => {
       fetchState.value.isWorking = true
@@ -34,8 +36,6 @@ export const useFetchExtended = <T>(
       fetchState.value.error = null
 
       if (!response.ok) {
-        // Handle globally here before rejecting - e.g call a toast
-        toast.show(response._data.message)
         fetchState.value.error = (response as Record<string, any>).message
 
         return Promise.reject(response)
@@ -49,9 +49,6 @@ export const useFetchExtended = <T>(
         }
 
         auth.value = authCookie.value
-        // This is optional, you can setup your pageIntended based on product use case
-        const pageIntended = useState('page_intended', () => '/')
-        navigateTo(pageIntended.value)
       }
 
       if (url === '/auth/logout') {
@@ -71,18 +68,18 @@ export const useFetchExtended = <T>(
       fetchState.value.error = error.message
 
       if (import.meta.client) {
-        if (navigator.onLine)
-          toast.show(error?.message)
-        else
-          toast.show('You are offline', useDefault('toastError'))
+        if (navigator.onLine) {
+          useToastExtended('error').show(error?.message)
+        }
+        else {
+          // toast.show('You are offline', useDefault('toastError'))
+        }
       }
     },
 
     onResponseError: ({ error }) => {
       fetchState.value.isWorking = false
       fetchState.value.error = error?.message
-
-      toast.show(`${error?.message} From here`)
     },
   }
 
--- composables/usePost.ts
+++ composables/usePost.ts
@@ -1,4 +1,8 @@
 import { useFetchExtended } from './useFetchExtended'
 
-export const usePost = <T>(url: string, payload: Record<string, any> = {}) =>
-  useFetchExtended<T>(url, { method: 'POST' }, payload)
+export const usePost = <T>(
+  url: string,
+  payload: Record<string, any> = {},
+  bypass?: Record<string, null>,
+  baseUrlType?: string,
+) => useFetchExtended<T>(url, { method: 'POST' }, payload, bypass, baseUrlType)
