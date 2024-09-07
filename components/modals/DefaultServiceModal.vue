c66cb9f1f335a2fcf1ad2d8866453860e78a3651
Keji Ojo <ojokeji@gmail.com>
2024-09-07 17:29:16 +0100

    update: fixed paystack error

--- components/modals/DefaultServiceModal.vue
+++ components/modals/DefaultServiceModal.vue
@@ -1,45 +1,39 @@
 <template>
   <Transition name="modal-fade">
     <div v-if="modal.isVisible" class="c-popup cc-show">
-    <div class="c-popup_inner cc-service-checkout">
-      <div class="popup-close_btn cc-white" @click="closeModal"></div>
-      <div class="heading-h4">{{selectedService.name}}</div>
-      <div class="services-details_block">
-        <div class="services-form_wrapper w-form">
-          <form id="wf-form-services-form" name="wf-form-services-form" data-name="services form" method="get" class="services-form_container" data-wf-page-id="66533123d0bbd2c82eeaa214" data-wf-element-id="e0bb01c6-2b6a-3e88-98da-6a5417ee327d">
-            <div id="scrollbar" data-lenis-prevent="" class="services-form_inner">
-              <div class="services-form_block">
-                <p class="services-from_header">{{fieldTitle}}</p>
-                <div class="c-form_field cc-sm" v-for="(field, index) in fields" :key="index" >
-                      <div class="c-label_wrapper">
-                        <label class="c-label">{{ field.label }}</label>
-                      </div>
-                      <component :is="field.type === 'textarea' ? 'textarea' : 'input'" :placeholder="field.placeholder"
-                        class="c-input w-input" :type="field.type" />
+      <div class="c-popup_inner cc-service-checkout">
+        <div class="popup-close_btn cc-white" @click="closeModal"></div>
+        <div class="heading-h4">{{ selectedService.name }}</div>
+        <div class="services-details_block">
+          <div class="services-form_wrapper w-form">
+            <form id="wf-form-services-form" name="wf-form-services-form" data-name="services form" method="get"
+              class="services-form_container" data-wf-page-id="66533123d0bbd2c82eeaa214"
+              data-wf-element-id="e0bb01c6-2b6a-3e88-98da-6a5417ee327d">
+              <div id="scrollbar" data-lenis-prevent="" class="services-form_inner">
+                <div class="services-form_block">
+                  <p class="services-from_header">{{ fieldTitle }}</p>
+                  <div class="c-form_field cc-sm" v-for="(field, index) in fields" :key="index">
+                    <div class="c-label_wrapper">
+                      <label class="c-label">{{ field.label }}</label>
                     </div>
-                                <div class="c-form_field cc-sm">
-      <div class="c-label_wrapper">
-        <label class="c-label">Phone Number</label>
-      </div>
-      <input
-        v-model="formData.phone_number"
-        type="tel"
-        placeholder="Enter your phone number"
-        class="c-input w-input"
-      />
-    </div>
-                                <div class="c-form_field cc-sm">
-      <div class="c-label_wrapper">
-        <label class="c-label">Email</label>
-      </div>
-      <input
-        v-model="formData.email"
-        type="email"
-        placeholder="Enter your phone number"
-        class="c-input w-input"
-      />
-    </div>
-                <!-- <div class="c-form_field cc-sm">
+                    <component :is="field.type === 'textarea' ? 'textarea' : 'input'" :placeholder="field.placeholder"
+                      class="c-input w-input" :type="field.type" />
+                  </div>
+                  <div class="c-form_field cc-sm">
+                    <div class="c-label_wrapper">
+                      <label class="c-label">Phone Number</label>
+                    </div>
+                    <input v-model="formData.phone_number" type="tel" placeholder="Enter your phone number"
+                      class="c-input w-input" />
+                  </div>
+                  <div class="c-form_field cc-sm">
+                    <div class="c-label_wrapper">
+                      <label class="c-label">Email</label>
+                    </div>
+                    <input v-model="formData.email" type="email" placeholder="Enter your phone number"
+                      class="c-input w-input" />
+                  </div>
+                  <!-- <div class="c-form_field cc-sm">
                   <div class="c-label_wrapper">
                     <div class="c-label">Company Name</div>
                   </div>
@@ -75,67 +69,70 @@
                   </div>
                   <div class="c-input_wrapper"><textarea id="Aims-Objectives-2" name="Aims-Objectives-2" maxlength="5000" data-name="Aims Objectives 2" placeholder="Enter Organisation Aims &amp; Objectives" required="" class="c-input cc-textarea w-input"></textarea></div>
                 </div> -->
-              </div>
-              <div class="services-form_block">
-                <p class="services-from_header">{{additionalTitle}}</p>
-                <div class="c-form_field cc-sm" v-for="(field, index) in additionalFields" :key="index" >
-                      <div class="c-label_wrapper">
-                        <label class="c-label">{{ field.label }}</label>
-                      </div>
-                      <component :is="field.type === 'textarea' ? 'textarea' : 'input'" :placeholder="field.placeholder"
-                        class="c-input w-input" :type="field.type" />
+                </div>
+                <div class="services-form_block">
+                  <p class="services-from_header">{{ additionalTitle }}</p>
+                  <div class="c-form_field cc-sm" v-for="(field, index) in additionalFields" :key="index">
+                    <div class="c-label_wrapper">
+                      <label class="c-label">{{ field.label }}</label>
                     </div>
-              </div>
-            </div>
-            <div class="btn-flex cc-order-popup"><input @click.prevent="handlePayment" type="submit" data-wait="" class="c-button w-button" value="Next"></div>
-          </form>
-          <div class="w-form-done">
-            <div>Thank you! Your submission has been received!</div>
-          </div>
-          <div class="w-form-fail">
-            <div>Oops! Something went wrong while submitting the form.</div>
-          </div>
-        </div>
-        <div class="service-order_details">
-          <div data-lenis-prevent="" class="download-reciept_block">
-            <div class="download-reciept">
-              <div class="order-details_top">
-                <div class="download-form_header uc-mb-0">ORDER DETAILS</div>
-                <div class="order-id"></div>
-                <div class="order-details">
-                  <div class="order-details_inner">
-                    <div>{{selectedService.name}}</div>
-                    <div class="order-price">{{selectedService.price}}</div>
+                    <component :is="field.type === 'textarea' ? 'textarea' : 'input'" :placeholder="field.placeholder"
+                      class="c-input w-input" :type="field.type" />
                   </div>
-                  <div class="order-code_image"><img src="@/public/assets/images/la_qrcode.png" loading="lazy" alt="" class="c-img"></div>
                 </div>
               </div>
-              <div class="order-details_bottom">
-                <div class="order-details_line"></div>
-                <div class="order-details_flex">
-                  <div class="order-price_title">SubTotal</div>
-                  <div>₦{{ numberEmptyState(formatNumber(selectedService?.price, "0,0")) }}</div>
-                </div>
-                <div class="order-details_flex">
-                  <div class="order-price_title">Paystack Transactions Fee</div>
-                  <div>₦{{ numberEmptyState(formatNumber(transactionFee, "0,0")) }}</div>
-                </div>
-                <div class="reciept-circle cc-green cc-left"></div>
-                <div class="reciept-circle cc-green cc-right"></div>
-              </div>
-              <div class="order-details_flex">
-                <div class="order-price_title">Total</div>
-                <div>₦{{ numberEmptyState(formatNumber(totalPrice, "0,0")) }}</div>
-              </div>
+              <div class="btn-flex cc-order-popup"><input @click.prevent="handlePayment" type="submit" data-wait=""
+                  class="c-button w-button" value="Next"></div>
+            </form>
+            <div class="w-form-done">
+              <div>Thank you! Your submission has been received!</div>
             </div>
-            <div class="c-note">
-              <div class="note-icon"></div>
-              <div class="note-text">Kindly wait for your order confirmation after payment before closing the browser, tab or this page</div>
+            <div class="w-form-fail">
+              <div>Oops! Something went wrong while submitting the form.</div>
+            </div>
+          </div>
+          <div class="service-order_details">
+            <div data-lenis-prevent="" class="download-reciept_block">
+              <div class="download-reciept">
+                <div class="order-details_top">
+                  <div class="download-form_header uc-mb-0">ORDER DETAILS</div>
+                  <div class="order-id"></div>
+                  <div class="order-details">
+                    <div class="order-details_inner">
+                      <div>{{ selectedService.name }}</div>
+                      <div class="order-price">{{ selectedService.price }}</div>
+                    </div>
+                    <div class="order-code_image"><img src="@/public/assets/images/la_qrcode.png" loading="lazy" alt=""
+                        class="c-img"></div>
+                  </div>
+                </div>
+                <div class="order-details_bottom">
+                  <div class="order-details_line"></div>
+                  <div class="order-details_flex">
+                    <div class="order-price_title">SubTotal</div>
+                    <div>₦{{ numberEmptyState(formatNumber(selectedService?.price, "0,0")) }}</div>
+                  </div>
+                  <div class="order-details_flex">
+                    <div class="order-price_title">Paystack Transactions Fee</div>
+                    <div>₦{{ numberEmptyState(formatNumber(transactionFee, "0,0")) }}</div>
+                  </div>
+                  <div class="reciept-circle cc-green cc-left"></div>
+                  <div class="reciept-circle cc-green cc-right"></div>
+                </div>
+                <div class="order-details_flex">
+                  <div class="order-price_title">Total</div>
+                  <div>₦{{ numberEmptyState(formatNumber(totalPrice, "0,0")) }}</div>
+                </div>
+              </div>
+              <div class="c-note">
+                <div class="note-icon"></div>
+                <div class="note-text">Kindly wait for your order confirmation after payment before closing the browser,
+                  tab or this page</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   </Transition>
 
@@ -144,28 +141,27 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import type { Services } from "~/types/categories"
-import { useModal } from "~/composables/useModal";
+import { useModal } from "~/composables/useModal"
 
-// import { usePay } from '/composables/usePay';
-const auth = useAuth();
-  const payment = usePay();
+const auth = useAuth()
+const payment = usePay()
 
 
-const transactionFee = ref<number>(10);
-const totalPrice = ref<number>(0);
+const transactionFee = ref<number>(10)
+const totalPrice = ref<number>(0)
 
 // const nameRegex = helpers.regex(/^[A-Za-z]+(?:\s[A-Za-z]+)*\s*$/);
 const props = defineProps({
   service: {
-   type: Object as () => Services,
-    default: () => {}
+    type: Object as () => Services,
+    default: () => { }
   }
 })
 
 const selectedService = ref<Services>(props.service)
 
 
-totalPrice.value = selectedService?.value.price + transactionFee.value;
+totalPrice.value = selectedService?.value.price + transactionFee.value
 
 
 const fields = ref<any[]>([])
@@ -176,11 +172,11 @@ const additionalTitle = ref<string>()
 
 const error = ref<string | null>(null)
 const formData = ref<Record<string, any>>({
-  phone_number: '', 
+  phone_number: '',
   email: '',
-});
+})
 
-const createOrderState = useFetchState("/orders/create");
+const createOrderState = useFetchState("/orders/create")
 
 
 
@@ -216,12 +212,12 @@ const fetchApiResponse = async () => {
 
 
 const createOrder = async () => {
- const phoneNumber = formData.value.phone_number;
-  const companyName = "";
-  const alternateCompanyName = "";
-  const registeredAddress = "";
-  const objectOfBusiness = "";
-  const scopeOfBusiness = "";
+  const phoneNumber = formData.value.phone_number
+  const companyName = ""
+  const alternateCompanyName = ""
+  const registeredAddress = ""
+  const objectOfBusiness = ""
+  const scopeOfBusiness = ""
   const payload = {
     service_id: selectedService?.value.id,
     phone_number: phoneNumber,
@@ -235,26 +231,26 @@ const createOrder = async () => {
       "Object of Business": objectOfBusiness,
       "Scope of Business": scopeOfBusiness,
     },
-  };
-  
+  }
 
 
-  const { data, error } = await usePost(createOrderState.value.url, payload);
-  if (data.value) useToastExtended("success").show("Success");
-  return data.value;
-};
+
+  const { data, error } = await usePost(createOrderState.value.url, payload)
+  if (data.value) useToastExtended("success").show("Success")
+  return data.value
+}
 
 const handlePayment = async () => {
-  if (auth.value?.isLoggedIn) {
+  if (auth.value?.isLoggedIn && modal.isVisible) {
     payWithPaystack()
   } else {
     modal.show("SignInModal")
-   modal.hide('DefaultServiceModal')
+    modal.hide('DefaultServiceModal')
   }
-};
+}
 
 const payWithPaystack = async () => {
-  const order = await createOrder();
+  const order = await createOrder()
 
 
   if (order) {
@@ -263,24 +259,24 @@ const payWithPaystack = async () => {
         formData.value.email,
         selectedService.value.price,
         order.data.payment_ref
-      );
-        modal.hide('DefaultServiceModal')
-        modal.show('DefaultOrderSuccessModal')
+      )
+      modal.hide('DefaultServiceModal')
+      modal.show('DefaultOrderSuccessModal')
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   }
-};
+}
 
 const generateRef = () => {
   const prefix = () => {
-    return "pstk";
-  };
+    return "pstk"
+  }
 
-  const randomNumber = Math.floor(100000000000 + Math.random() * 900000000000);
-  const randomId = prefix() + randomNumber;
-  return randomId;
-};
+  const randomNumber = Math.floor(100000000000 + Math.random() * 900000000000)
+  const randomId = prefix() + randomNumber
+  return randomId
+}
 
 
 
@@ -288,12 +284,12 @@ const generateRef = () => {
 const modal = useModal('DefaultServiceModal')
 
 const emit = defineEmits<{
-  (event: 'modalClosed', service: Services | null): void;
-}>();
+  (event: 'modalClosed', service: Services | null): void
+}>()
 
 const closeModal = () => {
   modal.hide('DefaultServiceModal')
-   emit('modalClosed', null);
+  emit('modalClosed', null)
 }
 
 onMounted(() => {
@@ -301,6 +297,4 @@ onMounted(() => {
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
